8afcb962ebbfb8ce9a3bd6f7fec3ce49385260f7
v.proskurin <v.proskurin@nexters.com>
2022-07-17 02:34:11 +0300

    cmake and testing

---
+++ src/test.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Вадим Проскурин on 09.05.2022.
+//
+
+#include "test.hpp"
+
+#include "instruments.pb.h"
+using namespace tinkoff::public_::invest::api::contract::v1;
+
+void test::t() {
+    auto* a = new Instrument();
+
+
+}
---
+++ src/test.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Вадим Проскурин on 09.05.2022.
+//
+
+
+#pragma once
+
+class test {
+public:
+    static void t();
+};
+
+
